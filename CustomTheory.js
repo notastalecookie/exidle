a398802b2136ce6db3b5b9faeb0b143f415d88f0
notastalecookie <101066527+notastalecookie@users.noreply.github.com>
2022-03-07 18:29:50 -0500

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -12,13 +12,32 @@ var authors = "notastalecookie";
 var version = 1;
 
 var currency1, currency2;
-var c1, c2, ;
-var c1Exp, c2Exp;
+var c_1, c_2, ;
+ //// filler, var ms upgrades
         
 var init = () => {
   currency1 = theory.createCurrency();
+  ///////////////////
+  // Regular Upgrades
+
+  // c_1
+{
+        let getDesc = (level) => "c_1="getc_1(level).toString(0)
+        c_1 = theory.createUpgrade(0, currency2, new FirstFreeCost(new Exponentialcost(15, Math.log2(2))))
+        c_1.getDescription = (amount) => Utils.getMath(getDesc(b1.level))
+        c_1.getInfo = (amount) => Utils.getMathTo(getDesc(b2.level), getDesc(b2.level + amount));
+}
         
-  currency2 = theory.createCurrency();
+  // c_2 
+{
+        let getDesc = (level) => "c_2=2^{" + level + "}";
+        let getInfo = (level) => "c_2=" + getC2(level).toString(0);
+        c_2 = theory.createUpgrade(1, currency2, new ExponentialCost(5, Math.log2(10)));
+        c_2.getDescription = (_) => Utils.getMath(getDesc(c_2.level));
+        c_2.getInfo = (amount) => Utils.getMathTo(getInfo(c_2.level), getInfo(c2.level + amount));
+    }
+
+ currency2 = theory.createCurrency();
 }
 theory.primaryEquationHeight = 120;
 var getprimaryEquation = () => {
