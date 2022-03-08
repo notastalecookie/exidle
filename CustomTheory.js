0cf9ca87bf55cbb563882d7c8c0ba3a1d1ff2138
notastalecookie <101066527+notastalecookie@users.noreply.github.com>
2022-03-07 21:31:31 -0500

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -65,6 +65,6 @@ var tick = (elapsedTime, multiplier) => {
 var getPublicationMultiplier = (tau) => tau.pow(0.147) * BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "3{" + symbol + "}^{1.47}";
 var getTau = () => currency1.value^0.1;
-var getc_1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 0);
+var getc_1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 1);
 var getc_2 = (level) => BigNumber.TWO.pow(level);
 init();
