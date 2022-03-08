fad1239bee509edd0b128f488d0e1fafb92bcb53
notastalecookie <101066527+notastalecookie@users.noreply.github.com>
2022-03-07 21:42:30 -0500

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -58,7 +58,7 @@ var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
  
-    currency1.value += dt * bonus * getc_1(c_1.level) * c_2;
+    currency1.value += dt * bonus * getc_1(c_1.level) * getc_2(c_2.level);
     currency2.value += currency1.value;
 }
 
