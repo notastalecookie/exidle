7f7184df1528133f96924b1f3ab62d683391a114
notastalecookie <101066527+notastalecookie@users.noreply.github.com>
2022-03-07 21:12:19 -0500

    Update CustomTheory.js

--- CustomTheory.js
+++ CustomTheory.js
@@ -5,14 +5,7 @@ import { theory } from "./api/Theory";
 import { Utils } from "./api/Utils";
 
 
-var id = "student_loans101";
-var name = "Student Loans";
-var description = "A theory based on student's t distribution and loaning rho to push your tau";
-var authors = "notastalecookie";
-var version = 1;
 
-var currency1, currency2;
-var c_1, c_2;
  //// filler, var ms upgrades
         
 var init = () => {
@@ -21,7 +14,23 @@ var init = () => {
   ///////////////////
   // Regular Upgrades
 
- /// PASTE HERE
+  // c_1
+{
+   let getDesc = (level) => "c_1=" + getc_1(level).toString(0);
+   c_1 = theory.createUpgrade(2, currency2, new ExponentialCost(15, Math.log2(2)));
+   c_1.getDescription = (amount) => Utils.getMath(getDesc(c_1.level));
+   c_1.getInfo = (amount) => Utils.getMathTo(getDesc(c_1.level), getDesc(c_1.level + amount));
+ 
+}
+        
+  // c_2 
+{
+        let getDesc = (level) => "c_2=2^{" + level + "}";
+        let getInfo = (level) => "c_2=" + getc_2(level).toString(0);
+        c_2 = theory.createUpgrade(1, currency2, new ExponentialCost(5, Math.log2(10)));
+        c_2.getDescription = (_) => Utils.getMath(getDesc(c_2.level));
+        c_2.getInfo = (amount) => Utils.getMathTo(getInfo(c_2.level), getInfo(c2.level + amount));
+    }
     theory.createPublicationUpgrade(0, currency2, 1e9);
     theory.createBuyAllUpgrade(1, currency2, 1e13);
     theory.createAutoBuyerUpgrade(2, currency2, 1e30);
